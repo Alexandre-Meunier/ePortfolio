0f158793d20296839bd0036d6889742991d759b7
Alexandre-Meunier <alexaudmeunier@outlook.fr>
2024-07-31 11:22:01 +0200

    Update Portfolio

--- fonctions.js
+++ fonctions.js
@@ -12,3 +12,18 @@ function flashingInputBar()  {
 }
 
 flashingInputBar();
+
+document.addEventListener("DOMContentLoaded", function(){
+    const elementsToShow = document.querySelectorAll('.apparition');
+    const observer = new IntersectionObserver((entries)=>{
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+                entry.target.style.opacity = 1;
+                observer.unobserve(entry.target);
+            }
+        });
+    },{threshold: 0.1});
+    elementsToShow.forEach(element=>{
+        observer.observe(element);
+    });
+})
